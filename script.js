c0a01ca7ab53536bef10d55e4fa0ad61c1572b17
GokulSB <142734265+Gokulcs-04@users.noreply.github.com>
2026-02-08 15:20:08 +0530

    Create the initial files and update structure

---
+++ script.js
@@ -0,0 +1,21 @@
+const myLibrary = [];
+
+function Book(title, author, pages, haveRead, id) {
+    if (!new.target) {
+        throw Error("Use 'new' operator to call the constructor");
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
+    this.id = id;
+    this.info = function () {
+        return `The title of the book is ${this.title}`;
+    }
+}
+
+function addBookToLibrary(title, author, pages, haveRead) {
+    const newBook = new Book(title, author, pages, haveRead , crypto.randomUUID());
+    myLibrary.push(newBook);
+}
+
