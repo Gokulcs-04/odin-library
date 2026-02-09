a1a361c90d0e754827ec73edef01614b19a3ccf6
GokulSB <142734265+Gokulcs-04@users.noreply.github.com>
2026-02-09 20:00:14 +0530

    Update Card addition and removal logic and css

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const myLibrary = [];
+let removeButtons;
 
 function Book(title, author, pages, haveRead, id) {
     if (!new.target) {
@@ -16,28 +17,26 @@ function Book(title, author, pages, haveRead, id) {
 }
 
 function addBookToLibrary(title, author, pages, haveRead) {
-    const newBook = new Book(title, author, pages, haveRead , crypto.randomUUID());
+    let newId = crypto.randomUUID();
+    const newBook = new Book(title, author, pages, haveRead , newId);
     myLibrary.push(newBook);
 }
 
-// myLibrary.push(
-//     new Book("1984", "George Orwell", 328, true, 1),
-//     new Book("The Hobbit", "J.R.R. Tolkien", 310, false, 2),
-//     new Book("To Kill a Mockingbird", "Harper Lee", 281, true, 3),
-//     new Book("The Catcher in the Rye", "J.D. Salinger", 277, false, 4),
-//     new Book("Dune", "Frank Herbert", 412, false, 5),
-//     new Book("1984", "George Orwell", 328, true, 1),
-//     new Book("The Hobbit", "J.R.R. Tolkien", 310, false, 2),
-//     new Book("To Kill a Mockingbird", "Harper Lee", 281, true, 3),
-//     new Book("The Catcher in the Rye", "J.D. Salinger", 277, false, 4),
-//     new Book("Dune", "Frank Herbert", 412, false, 5)
-// );
+myLibrary.push(
+    new Book("1984", "George Orwell", 328, true, 1),
+    new Book("The Hobbit", "J.R.R. Tolkien", 310, false, 2),
+    new Book("To Kill a Mockingbird", "Harper Lee", 281, true, 3),
+    new Book("The Catcher in the Rye", "J.D. Salinger", 277, false, 4),
+    new Book("Dune", "Frank Herbert", 412, false, 5),
+
+);
 
 
 const submitButton = document.querySelector(".submit");
 const formElement = document.querySelector('form');
 const clearButton = document.querySelector('.clear');
 const library = document.querySelector('.library');
+
 
 submitButton.addEventListener('click', (e) => {
     e.preventDefault();
@@ -49,20 +48,53 @@ submitButton.addEventListener('click', (e) => {
     let readStatus = formData.get('read');
 
     addBookToLibrary(title, author, pages, readStatus);
+    displayBooksInArray();
     console.log(myLibrary);
     formElement.reset();
 });
 
+
+function displayBooksInArray() {
+    library.innerHTML = '';
+    myLibrary.forEach((book) => {
+        addCard(book);
+    })
+    addRemoveListener();
+}
+
 function addCard(book) {
-    library.innerHTML += `<div class="card">
+    library.innerHTML += `<div class="card" data-id=${book.id}">
                 <h3 class="title-card">${book.title}</h3>
                 <p class="author-card">Author: <span>${book.author}</span></p>
                 <p class="pages-card">Pages: <span>${book.pages}</span></p>
                 <p class="read-card">Read Status:${book.haveRead ? book.haveRead: "not read"}</span></p>
-                <button>Remove</button>
+                <button class="remove-button" id=${book.id}>Remove</button>
             </div>`
 }
 
-// myLibrary.forEach((book) => {
-//     addCard(book);
-// })
+function addRemoveListener() {
+    removeButtons = document.querySelectorAll('.remove-button');
+
+    removeButtons.forEach((removeButton) => {
+        removeButton.addEventListener('click', (e) => {
+            removeBookFromArray(e);
+        }) 
+    })
+}
+
+function removeBookFromArray(e) {
+    let targetId = e.target.id;
+    console.log(e.target);
+    const indexToRemove = myLibrary.findIndex(item => item.id == targetId);
+
+    if (indexToRemove !== -1 ) {
+        myLibrary.splice(indexToRemove, 1);
+    }
+    displayBooksInArray();
+}
+
+displayBooksInArray();
+
+
+
+
